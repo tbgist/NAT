2dbfd44ff273132fa100b398fb0a643304e49e29
BinTang <1460066016@qq.com>
2022-11-22 14:08:51 +0800

    v0.14

--- src/designer/MainWindow.py
+++ src/designer/MainWindow.py
@@ -1,7 +1,7 @@
 import sys
 
 from src.designer.UI_MainWindow import Ui_MainWindow
-from PyQt5.QtWidgets import QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QHeaderView
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from scapy.all import *
 from scapy.layers.inet import Ether, TCP, UDP, ICMP
@@ -17,9 +17,10 @@ class MainWindow(Ui_MainWindow):
         self.model = QStandardItemModel()
         self.model.setHorizontalHeaderLabels(["type", "src", "dst"])
         self.PackageList.setModel(self.model)
+        self.PackageList.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         # 设置默认文件路径
         self.save_path = "../../packages/"
-        self.file_path = self.save_path + "long2.pcap"
+        self.is_start = False
         # 打开文件
         self.actionopen.triggered.connect(self.open)
         # 查看报文信息
@@ -29,18 +30,24 @@ class MainWindow(Ui_MainWindow):
         self.actionsave.triggered.connect(self.save)
         # 另存为
         self.actionsave_as.triggered.connect(self.save_as)
-        # 点击开始按钮开始抓包
-        # self.Start.clicked.connect(self.start)
-        # # 点击暂停按钮暂停抓包
-        # self.pause()
-        # # 点击终止按钮结束抓包
-        # self.pause()
+        # 点击开始按钮开始/暂停抓包
+        self.Start.clicked.connect(self.start)
 
     def start(self):
-        pass
+        if self.Start.text() == "开始":
+            self.Start.setText("暂停")
+            # 开始抓包
+            self.is_start = True
+            # start_sniff(True)
+            # read()
+        else:
+            self.Start.setText("开始")
+            self.is_start = False
+            # start_sniff(False)
 
-    def pause(self):
-        pass
+    def read(self):
+        while self.is_start:
+            pass  # 读取临时文件
 
     def show(self):
         self.mainwindow.show()
@@ -49,11 +56,11 @@ class MainWindow(Ui_MainWindow):
         ind = index.row()
         p = self.pkt[ind]
         saved_stdout = sys.stdout
-        with open('temp.txt', 'w+') as file:
+        with open('temp.txt', 'w+', encoding="utf8") as file:
             sys.stdout = file
             p.show()
         sys.stdout = saved_stdout
-        with open('temp.txt', 'r') as file:
+        with open('temp.txt', 'r', encoding="utf8") as file:
             details = file.read()
         self.Details.setText(details)
         info = ""
@@ -67,6 +74,7 @@ class MainWindow(Ui_MainWindow):
 
     def save_as(self):
         path = QFileDialog.getSaveFileName(self.mainwindow, '保存文件', '', 'Pcap Files (*.pcap)')
+        wrpcap(path[0], self.pkt)
 
     def open(self):
         path, _ = QFileDialog.getOpenFileNames(self.mainwindow, "打开文件", self.save_path, "Pcap Files (*.pcap)")
